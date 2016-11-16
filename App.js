03162723785353ffcaac6728d0712fcb2755b88b
drector1 <rector.danielk@gmail.com>
2016-11-15 23:06:59 -0500

    hello world

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+
+//		class component can have state
 class App extends React.Component {
 	render(){
-		return <div>Hi</div>
+		return <h1>Hello World</h1>
 	}
 }
+
+
+//		constant variable (stateless function component) does not have state
+// const App = () => <h1>Hello All</h1>
 
 export default App

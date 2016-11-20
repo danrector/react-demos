16f6d8ecb27a7410bb304066d2646e547365df45
drector1 <rector.danielk@gmail.com>
2016-11-19 21:37:06 -0500

    -created text input form -set up state to update whenever the input field changes, rendering the text in <h1>

--- App.js
+++ App.js
@@ -9,8 +9,17 @@ class App extends React.Component {
 		super();
 		this.state = { txt: 'this is the state txt'}
 	}
+	update(e){
+		this.setState({txt: e.target.value})
+	}
 	render(){
-		return <h1>{this.state.txt}</h1>
+		return (
+		<div>
+		<input type="text" 
+			onChange={this.update.bind(this)} />
+		<h1>{this.state.txt}</h1>
+		</div>
+		);
 	}
 }
 

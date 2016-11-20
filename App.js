0e15afc43fc197933821637e69282e01c6293b4b
drector1 <rector.danielk@gmail.com>
2016-11-19 20:52:17 -0500

    changed app to render from app.js

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 //		class component can have state
 
 //		rendering two elements must be done using a wrapper.
 class App extends React.Component {
 	render(){
-		return (
-			<div>
-			<h1>Hello World</h1>
-			<b>Bold</b>
-			</div>
-		);
+		
+		return <h1>{this.props.txt}</h1>
 	}
 }
 
+ReactDOM.render(
+	<App txt="this is the props text" />,
+	document.getElementById('app')
+);
 
 //		constant variable (stateless function component) does not have state
 // const App = () => <h1>Hello All</h1>
 
-export default App
+//export default App

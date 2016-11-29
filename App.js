d42a28bdc84d11d59b636936b3c62eaa6cd8155f
drector1 <rector.danielk@gmail.com>
2016-11-29 18:19:44 -0500

    -created button component that changes display based on state

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = { val: 0 };
+		this.update = this.update.bind(this);
+	}
+	update(){
+		this.setState({val: this.state.val + 1})
+	}
 	render(){
-		return <Button>I <Heart/> React</Button>
+		console.log('rendering!')
+		return <button onClick={this.update}>{this.state.val}</button>
 	}
 }
-
-class Button extends React.Component {
-	render(){
-		return <button>{this.props.children}</button>
-	}
-}
-
-const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 export default App
 
+// class Button extends React.Component {
+// 	render(){
+// 		return <button>{this.props.children}</button>
+// 	}
+// }
 
-
-
+// const Heart = () => <span className="glyphicon glyphicon-heart"></span>
 
 
 

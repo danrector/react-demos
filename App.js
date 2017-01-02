b149845b4abfd8c598fe89631e3672614c4380f8
drector1 <rector.danielk@gmail.com>
2017-01-02 15:42:56 -0500

    init app set up, made button and label

--- App.js
+++ App.js
@@ -1,39 +1,69 @@
 import React from 'react';
 
 class App extends React.Component {
-	constructor(){
-		super();
-		this.state = {items: []}
-	}
-	componentWillMount(){
-		fetch( 'http://swapi.co/api/people/?format=json' )
-			.then( response => response.json() )
-			.then( ({results: items}) => this.setState({items}))
-	}
-	filter(e){
-		this.setState({filter: e.target.value})
-	}
 	render(){
-		let items = this.state.items;
-		if(this.state.filter){
-			items = items.filter( item =>
-				item.name.toLowerCase()
-				.includes(this.state.filter.toLowerCase()))
-		}
-		return(
+		return (
 			<div>
-				<input type="text" 
-				onChange={this.filter.bind(this)}/>
-				{items.map(item => 
-					<Person key={item.name} person={item} />)}
+				<Button>button</Button>
+				<hr/>
+				<Label>label</Label>
 			</div>
 		)
 	}
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+const Button = (props) => <button>{props.children}</button>
+
+class Label extends React.Component {
+	render(){
+		return (
+			<label>{this.props.children}</label>
+		)
+	}
+}
 
 export default App
+
+
+
+
+
+//14
+
+// class App extends React.Component {
+// 	constructor(){
+// 		super();
+// 		this.state = {items: []}
+// 	}
+// 	componentWillMount(){
+// 		fetch( 'http://swapi.co/api/people/?format=json' )
+// 			.then( response => response.json() )
+// 			.then( ({results: items}) => this.setState({items}))
+// 	}
+// 	filter(e){
+// 		this.setState({filter: e.target.value})
+// 	}
+// 	render(){
+// 		let items = this.state.items;
+// 		if(this.state.filter){
+// 			items = items.filter( item =>
+// 				item.name.toLowerCase()
+// 				.includes(this.state.filter.toLowerCase()))
+// 		}
+// 		return(
+// 			<div>
+// 				<input type="text" 
+// 				onChange={this.filter.bind(this)}/>
+// 				{items.map(item => 
+// 					<Person key={item.name} person={item} />)}
+// 			</div>
+// 		)
+// 	}
+// }
+
+// const Person = (props) => <h4>{props.person.name}</h4>
+
+// export default App
 
 
 
